4b70720ae4d32bb6aaafa968d21c557fbac4ed83
Shen Chen Xu <shenchenxu@gmail.com>
2020-12-06 20:53:48 -0800

    More.

---
+++ 2020/rs/src/day6.rs
@@ -0,0 +1,33 @@
+use crate::utils;
+use std::collections::HashSet;
+
+pub fn day6(input_path: &str) {
+    let lines = utils::input_lines(input_path);
+    let mut unions: Vec<HashSet<char>> = Vec::new();
+    let mut intersections: Vec<HashSet<char>> = Vec::new();
+    let mut group: Vec<HashSet<char>> = Vec::new();
+
+    unions.push(HashSet::new());
+    for line in lines {
+        if line.is_empty() {
+            unions.push(HashSet::new());
+            intersections.push(group.iter().fold(group.first().unwrap().clone(), |x, y| {
+                x.intersection(y).cloned().collect::<HashSet<char>>()
+            }));
+            group = Vec::new();
+        } else {
+            let mut answer: HashSet<char> = HashSet::new();
+            for c in line.chars() {
+                unions.last_mut().unwrap().insert(c);
+                answer.insert(c);
+            }
+            group.push(answer);
+        }
+    }
+    intersections.push(group.iter().fold(group.first().unwrap().clone(), |x, y| {
+        x.intersection(y).cloned().collect::<HashSet<char>>()
+    }));
+
+    assert_eq!(unions.iter().map(|a| a.len()).sum::<usize>(), 6161);
+    assert_eq!(intersections.iter().map(|a| a.len()).sum::<usize>(), 2971);
+}
--- 2020/rs/src/main.rs
+++ 2020/rs/src/main.rs
@@ -3,12 +3,20 @@ mod day2;
 mod day3;
 mod day4;
 mod day5;
+mod day6;
 mod utils;
 
 use std::env;
 
 fn main() {
-    let days = [day1::day1, day2::day2, day3::day3, day4::day4, day5::day5];
+    let days = [
+        day1::day1,
+        day2::day2,
+        day3::day3,
+        day4::day4,
+        day5::day5,
+        day6::day6,
+    ];
     let args: Vec<String> = env::args().collect();
     if args.len() > 1 {
         let day = args[1].parse::<usize>().unwrap();
